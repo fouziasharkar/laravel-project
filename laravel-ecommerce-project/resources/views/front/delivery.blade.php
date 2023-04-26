cd70bc81cafb7a34b53df596b1dc837fb04ef805
fouziasharkar <114256129+fouziasharkar@users.noreply.github.com>
2023-04-26 12:11:53 +0600

    first commit

---
+++ laravel-ecommerce-project/resources/views/front/delivery.blade.php
@@ -0,0 +1,44 @@
+@extends('front.layout.layout')
+
+
+
+@section('content')
+
+<div class="span9" id="mainCol">
+    <ul class="breadcrumb">
+		<li><a href="index.html">Home</a> <span class="divider">/</span></li>
+		<li class="active">Page Title</li>
+    </ul>
+	<h3> Delivery</h3>	
+	<hr class="soft"/>
+	<h5>How to place order?</h5><br/>
+	<p>
+	Anyone can place order by just choosing products from the home page
+</p>
+<h5>Discount</h5><br/>
+<p>
+offers will be provided on selected products
+</p>
+<h5>Wrong Item </h5><br/>
+<p>
+In case of wrong item delivery the buyer will be able to contact us
+	</p>
+	<h5>Cart Edit</h5><br/>
+<p>
+Users will be able to add and remove products from the list
+</p>
+<h5>Payment</h5><br/>
+<p>
+Multiple options for delivery are available
+	</p>
+	<h5>Cash on Delivery</h5><br/>
+<p>
+In case of emergency, customers can take cash on deliveries but have to inform us beforehand by calling
+</p>
+<h5>Thank You</h5><br/>
+<p>
+Thank you all for considering our site for your services
+	</p>
+</div>
+
+@endsection
